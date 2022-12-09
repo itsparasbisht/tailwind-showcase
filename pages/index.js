e38b908164efffa1fcdaa51a29c198c7bc2e0235
itsparasbisht <iamparasbisht@gmail.com>
2022-12-09 20:27:01 +0530

    navbar

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,20 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div className="h-screen w-screen bg-[url('/camera.jpg')] bg-no-repeat bg-cover">
-      <nav className="bg-red-300 w-full h-20">
-        <img src="/logo.png" alt="" />
-        <div>
-          <span></span>
+      <nav className="w-ful p-4 flex justify-between items-center">
+        <Image src="/logo.png" alt="logo" width={"120"} height={"20"} />
+        <div className="text-[0.9rem] w-[50%] flex justify-around">
+          <span>EOS Rebel</span>
+          <span>EOS 6D</span>
+          <span>EOS 5D</span>
+          <span>EOS-1D X</span>
+          <span>EOS R7</span>
+          <span>EOS M50</span>
+        </div>
+        <div className="text-[0.9rem] w-[18%] flex justify-around">
+          <span>Shop</span>
+          <span>Account</span>
+          <span>Menu</span>
         </div>
       </nav>
     </div>
