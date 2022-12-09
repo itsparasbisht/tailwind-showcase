104b8f502be99cdbe625a8ef4d474ac23a515f61
itsparasbisht <iamparasbisht@gmail.com>
2022-12-09 20:36:14 +0530

    layer components

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="h-screen w-screen bg-[url('/camera.jpg')] bg-no-repeat bg-cover">
       <nav className="w-ful p-4 flex justify-between items-center">
         <Image src="/logo.png" alt="logo" width={"120"} height={"20"} />
-        <div className="text-[0.9rem] w-[50%] flex justify-around">
+        <div className="text-[0.9rem] w-[50%] flex justify-around font-semibold cursor-pointer">
           <span>EOS Rebel</span>
           <span>EOS 6D</span>
           <span>EOS 5D</span>
@@ -15,10 +15,10 @@ export default function Home() {
           <span>EOS R7</span>
           <span>EOS M50</span>
         </div>
-        <div className="text-[0.9rem] w-[18%] flex justify-around">
-          <span>Shop</span>
-          <span>Account</span>
-          <span>Menu</span>
+        <div className="text-[0.9rem] w-[18%] flex justify-around font-semibold cursor-pointer">
+          <span className="nav-btn-effect">Shop</span>
+          <span className="nav-btn-effect">Account</span>
+          <span className="nav-btn-effect">Menu</span>
         </div>
       </nav>
     </div>
